22b9034132de3b3df166eddbdbc9f7eb504dea83
Kunaije <kunaijekk@gmail.com>
2020-05-03 21:18:11 +0200

    render dom elements and working with Bulma CSS

--- index.js
+++ index.js
@@ -13,37 +13,70 @@ const fetchData = async (searchTerm) => {
 	return response.data.Search;
 };
 
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
+createAutoCompte({
+	root: document.querySelector('.autocomplete')
+});
 
-<label><b>Search For a Movie</b></label>
-<input class="input" />
-<div class="dropdown">
-	<div class="dropdown-menu">
-		<div class="dropdown-content results"></div>
-	</div>
-</div>
+createAutoCompte({
+	root: document.querySelector('.autocomplete-two')
+});
 
-`;
+createAutoCompte({
+	root: document.querySelector('.autocomplete-three')
+});
 
-const input = document.querySelector('input');
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
+const onMovieSelect = async (movie) => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: 'cef8ff80',
+			i: movie.imdbID
+		}
+	});
 
-const onInput = async (event) => {
-	const movies = await fetchData(event.target.value);
-
-	dropdown.classList.add('is-active');
-	for (let movie of movies) {
-		const option = document.createElement('a');
-
-		option.classList.add('dropdown-item');
-		option.innerHTML = `
-		<img src="${movie.Poster}" />
-		${movie.Title}
-	  `;
-
-		resultsWrapper.appendChild(option);
-	}
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data);
 };
-input.addEventListener('input', debounce(onInput, 500));
+
+const movieTemplate = (movieDetail) => {
+	return `
+	<article class="media">
+		<figure class="media-left">
+			<p class="image">
+				<img src="${movieDetail.Poster}" />
+			</p>
+		</figure>
+
+		<div class="media-content">
+			<div class	="content">
+				<h1>${movieDetail.Title}</h1>
+				<h4>${movieDetail.Genre}</h4>
+				<p>${movieDetail.Plot}</p>
+			</div>
+		</div> 
+	</article>
+
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.Awards}</p>
+		<p class="subtitle">Awards</p>
+	</article>
+
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.BoxOffice}</p>
+		<p class="subtitle">Box Office</p>
+	</article>
+
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.Metascore}</p>
+		<p class="subtitle">Metascore</p>
+	</article>
+
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.imdbRating}</p>
+		<p class="subtitle">IMDB Rating</p>
+	</article>
+
+	<article class="notification is-primary">
+		<p class="title">${movieDetail.imdbVotes}</p>
+		<p class="subtitle">IMDB Votes</p>
+	</article>
+	`;
+};
